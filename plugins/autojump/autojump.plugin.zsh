ab1fef932e80d9018195452c9604692f0cbdc0b3
Erwin van Eijk <erwin.vaneijk@gmail.com>
2013-10-28 20:59:43 +0100

    Added FreeBSD ports support

--- plugins/autojump/autojump.plugin.zsh
+++ plugins/autojump/autojump.plugin.zsh
@@ -9,6 +9,8 @@ if [ $commands[autojump] ]; then # check if autojump is installed
     . /usr/local/share/autojump/autojump.zsh
   elif [ -f $HOME/.autojump/etc/profile.d/autojump.zsh ]; then # manual user-local installation
     . $HOME/.autojump/etc/profile.d/autojump.zsh
+  elif [ -f /usr/local/share/autojump/autojump.zsh ]; then # freebsd ports
+    . /usr/local/share/autojump/autojump.zsh
   elif [ -f /opt/local/etc/profile.d/autojump.zsh ]; then # mac os x with ports
     . /opt/local/etc/profile.d/autojump.zsh
   elif [ $commands[brew] -a -f `brew --prefix`/etc/autojump.zsh ]; then # mac os x with brew
