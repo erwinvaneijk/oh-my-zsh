2d2935acec31297daeb4b6cece4dad1dce9a0bfc
Erwin van Eijk <235739+erwinvaneijk@users.noreply.github.com>
2021-03-14 15:05:08 +0100

    Updated aliases when exa available.

--- plugins/common-aliases/common-aliases.plugin.zsh
+++ plugins/common-aliases/common-aliases.plugin.zsh
@@ -6,16 +6,24 @@ export QUOTING_STYLE=literal
 # ls, the common ones I use a lot shortened for rapid fire usage
 if [ -x /usr/bin/exa ]; then
     alias ls='/usr/bin/exa'
+    alias l='exa -lbF --git'
+    alias ll='exa -lbGF --git'
+    alias llm='exa -lbGF --git --sort=modified'
+    alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'
+    alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale'
+    alias lS='exa -1'
+    alias lt='exa --tree --level=2'
+else
+    alias l='ls -lFh'     #size,show type,human readable
+    alias la='ls -lAFh'   #long list,show almost all,show type,human readable
+    alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
+    alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
+    alias ll='ls -l'      #long list
+    alias ldot='ls -ld .*'
+    alias lS='ls -1FSsh'
+    alias lart='ls -1Fcart'
+    alias lrt='ls -1Fcrt'
 fi
-alias l='ls -lFh'     #size,show type,human readable
-alias la='ls -lAFh'   #long list,show almost all,show type,human readable
-alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
-alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
-alias ll='ls -l'      #long list
-alias ldot='ls -ld .*'
-alias lS='ls -1FSsh'
-alias lart='ls -1Fcart'
-alias lrt='ls -1Fcrt'
 
 alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc' # Quick access to the .zshrc file
 
